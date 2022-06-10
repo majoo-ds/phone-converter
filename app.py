3f605e4d740ede4f3b387ed48d73ece32c002ef7
unknown <muhammad.ridwan.majoo.id>
2022-06-10 08:49:13 +0700

    second commit

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from turtle import title
 import streamlit as st
+
+st.title("Phone Number Converter")
+st.subheader("Please, review this guideline before uploading yout file.")
+st.markdown("1. Make sure file has no skipping rows")
+st.markdown("2. It'd be better to have a single sheet in it")
+st.markdown("3. If it has multiple sheets, make sure they're all in same structure.")
